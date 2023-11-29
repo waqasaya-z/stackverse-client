703178c6d8fc36abab393761d95fdb4d8074eadf
waqasaya-z <waqasayaz@icloud.com>
2023-11-30 00:41:46 +0500

    useMutation

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,13 +1,9 @@
 "use client";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { useRouter } from "next/navigation";
-import React, {
-  Dispatch,
-  SetStateAction,
-  SyntheticEvent,
-  useState
-} from "react";
+import { Dispatch, SetStateAction, useCallback } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 
 type LoginForm = {
   formEmail: string;
@@ -19,9 +15,6 @@ const HomePage = ({
 }: {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
-  const [errorMessage, setErrorMessage] = useState();
-  const [loading, setLoading] = useState(false);
-
   const {
     register,
     handleSubmit,
@@ -30,18 +23,29 @@ const HomePage = ({
 
   const router = useRouter();
 
-  const onSubmit: SubmitHandler<LoginForm> = (data) => {
-    setLoading(true);
-    axios
-      .post("http://localhost:3001/api/users", data)
-      .then((res) => {
-        router.push("/home");
-      })
-      .catch((err) => {
-        setErrorMessage(err.response.data);
-        setLoading(false);
-      });
+  const validateUser = async (data: LoginForm) => {
+    const response = await axios.post<LoginForm>(
+      "http://localhost:3001/api/users",
+      data
+    );
+
+    return response;
   };
+
+  const mutation = useMutation<AxiosResponse<any, any>, Error, LoginForm>(
+    validateUser
+  );
+
+  const onSubmit: SubmitHandler<LoginForm> = useCallback(
+    (data: LoginForm) => {
+      mutation.mutate(data, {
+        onSuccess: (data, variables, context) => {
+          router.push("/home");
+        }
+      });
+    },
+    [mutation, router]
+  );
 
   return (
     <div className="h-2/3 ml-40 text-center shadow-xl">
@@ -49,8 +53,13 @@ const HomePage = ({
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col font-medium gap-2 w-full px-10 pt-10 mb-4"
       >
-        {errorMessage && (
-          <p className="text-red-700 font-semibold text-sm"> {errorMessage} </p>
+        {mutation.error && (
+          <p className="text-red-700 font-semibold text-sm">
+            {" "}
+            {(mutation.error as any)?.response?.data ||
+              (mutation.error as Error)?.message ||
+              "An error occurred"}
+          </p>
         )}
         <div className="gap-2">
           <label htmlFor="email"> Username </label>
@@ -77,7 +86,7 @@ const HomePage = ({
         </div>
         <button className="border hover:border-black p-2 mt-2 rounded-lg">
           {" "}
-          {loading ? (
+          {mutation.isLoading ? (
             <span className="loading loading-infinity loading-md"></span>
           ) : (
             "Log in"
